334e412ae65724249171a2f3f7e99162ebdfb4ba
LeonardGecko <t16510ra@sfc.keio.ac.jp>
2019-09-22 01:36:51 +0900

    update run file

--- wordgen.py
+++ wordgen.py
@@ -18,8 +18,3 @@ for station in root.xpath('//station'):
 print(l_station)
 s = random.choice(l_station);
 print(s)
-#  print("station_cd:"+station.findtext('station_cd'))
-#  print("station_name:"+station.findtext('station_name'))
-#  print("longitude:"+station.findtext('lon'))
-#  print("latitude:"+station.findtext('lat'))
-#  print("----------")
